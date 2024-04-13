45a7c77f2f8737b322b77de0abe12500aec811f0
Hennadii Polupan <146132514+genvarius79@users.noreply.github.com>
2024-04-13 19:55:58 +0100

    validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,32 +1,54 @@
-import { Formik, Form, Field } from 'formik';
-import { useId } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { nanoid } from 'nanoid';
+import * as Yup from 'yup';
+
 import css from './ContactForm.module.css';
+const FeedbackSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+
+  number: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+});
 
 const initialValues = {
   name: '',
   number: '',
-  id: 0,
 };
 
 export default function ContactForm({ onAdd }) {
-  const nameId = useId();
-  const numberId = useId();
+  const nameId = nanoid();
+  const numberId = nanoid();
   const handleSubmit = (values, actions) => {
-    onAdd(values);
+    onAdd({
+      name: values.name,
+      number: values.number,
+      id: nameId,
+    });
     actions.resetForm();
   };
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={FeedbackSchema}
+    >
       <Form className={css.form}>
         <label className={css.lable} htmlFor={nameId}>
           Name
         </label>
         <Field className={css.field} type="text" name="name" id={nameId} />
+        <ErrorMessage name="name" component="span" />
 
         <label className={css.lable} htmlFor={numberId}>
           Number
         </label>
         <Field className={css.field} type="tel" name="number" id={numberId} />
+        <ErrorMessage name="number" component="span" />
         <button className={css.btn} type="submit">
           Add contact
         </button>
